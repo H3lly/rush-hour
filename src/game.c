c51adf6b250aa462a97dfc6fb96e117ed2240949
Camille Aussignac <camille.aussignac@gmail.com>
2016-03-03 10:39:08 +0100

    correction game.c

--- src/game.c
+++ src/game.c
@@ -45,6 +45,13 @@ bool equals(cpiece p1, cpiece p2) {
     return (get_x(p1) == get_x(p2))&&(get_y(p1) == get_y(p2))&&(get_width(p1) == get_width(p2))&&(get_height(p1) == get_height(p2));
 }
 
+//vérifie que la pièce ne dépace pas de la grille
+bool out_of_grid(cgame g, int piece_num){
+    int abs = get_x(game_piece(g, piece_num));
+    int ord = get_y(game_piece(g, piece_num));
+    return (abs<0||abs>5||ord<0||ord>5);
+}
+
 //ajouter vérification pour pas depasser les bords
 bool play_move(game g, int piece_num, dir d, int distance) {
     piece p = g->liste_piece[piece_num];
@@ -82,24 +89,15 @@ bool play_move(game g, int piece_num, dir d, int distance) {
         case RIGHT:
             move_piece(p, RIGHT, distance);
             for (int i = 0; i < g->nb_pieces; ++i) {
-                if (equals(p, (game_piece(g, i)))) || intersect(p, game_piece(g, i) || out_of_grid(g, i)) {
+                if ((equals(p, (game_piece(g, i)))) || intersect(p, game_piece(g, i) || out_of_grid(g, i))){
                     move_piece(p, LEFT, distance);
                     return false;
                 }
-                //g->nb_moves+=1;
-                //distance--;
             }
             g->nb_moves += distance;
             return true;
     }
     return false; 
-}
-
-//vérifie que la pièce ne dépace pas de la grille
-bool out_of_grid(game g, int piece_num){
-    int abs = g->liste_piece[piece_num]->abs;
-    int ord = g->liste_piece[piece_num]->ord;
-    return(abs<0||abs>5||ord<0||ord>5);
 }
 
 //retourne le nombre de mouvements effectués
