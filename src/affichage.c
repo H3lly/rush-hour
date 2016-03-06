ac7bd957a6a42fd37d64426575a9f604d3e1060b
Camille Aussignac <camille.aussignac@gmail.com>
2016-03-06 20:43:44 +0100

    correction affichage.c

--- src/affichage.c
+++ src/affichage.c
@@ -35,41 +35,60 @@ int main(void) {
         
         printf("Veuillez saisir le numéro de la pièce à déplacer :\n");
         while(!test){
-            scanf("%d", &piece_num);
-            if(piece_num>=0&&piece_num<game_nb_pieces(game))
-                test=true;
-            else
+            if (scanf("%d", &piece_num) == 1) {
+                if(piece_num>=0&&piece_num<game_nb_pieces(game))
+                  test=true;
+                else
+                    printf("Entrée invalide: veuillez saisir un entier 0 et %d.\n", game_nb_pieces(game)-1);
+            }
+            else{
                 printf("Entrée invalide: veuillez saisir un entier 0 et %d.\n", game_nb_pieces(game)-1);
+                while(fgetc(stdin)!='\n');
+            }
         }
         test=false;
         
         printf("Quelle direction ? (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
         while (!test) {
-            scanf("%d", &num);
-            if (num == 1) {
-                d = UP;
-                test = true;
-            } else if (num == 2) {
-                d = DOWN;
-                test = true;
-            } else if (num == 3) {
-                d = LEFT;
-                test = true;
-            } else if (num == 4) {
-                d = RIGHT;
-                test = true;
-            } else {
+            if (scanf("%d", &num) == 1) {
+                if (num == 1) {
+                    d = UP;
+                    test = true;
+                } else if (num == 2) {
+                    d = DOWN;
+                    test = true;
+                } else if (num == 3) {
+                    d = LEFT;
+                    test = true;
+                } else if (num == 4) {
+                    d = RIGHT;
+                    test = true;
+                } else {
+                    printf("Entrée invalide: veuillez saisir un entier entre 1 et 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+                }
+            }
+            else{
                 printf("Entrée invalide: veuillez saisir un entier entre 1 et 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+                while(fgetc(stdin)!='\n');
+//                do {
+//                    c = getchar();
+//                } while (!isdigit(c));
+//                ungetc(c, stdin);
             }
         }
-        test=false;
+        test=false;zg
         printf("De combien de cases ?\n");
         while(!test){
-            scanf("%d", &distance);
-            if(distance>0)
-                test=true;
-            else
+            if (scanf("%d", &distance)==1){
+                if(distance>0)
+                    test=true;
+                else
+                    printf("Entrée invalide: veuillez saisir un entier entre 0.\n");
+            }
+            else{
                 printf("Entrée invalide: veuillez saisir un entier superieur à 0.\n");
+                while(fgetc(stdin)!='\n');
+            }
         }
         move(grid, piece_num, d, distance);
     }
