6d38059cdf2e70567f4d9ebde6b57a50009adbd9
Camille Aussignac <camille.aussignac@gmail.com>
2016-03-07 22:57:40 +0100

    amélioration de game_test

--- src/game_test1.c
+++ src/game_test1.c
@@ -6,36 +6,35 @@
 
 #define NB_PIECES 4
 
- //On teste si value == expected avec des int
+//On teste si value == expected avec des int
 bool test_equality_int(int expected, int value, char * msg) {
-    if (expected != value)
+    if (expected!=value)
         fprintf(stderr, "ERR: value expected %d ; value computed %d. %s\n", expected, value, msg);
-    return expected == value;
+    return expected==value;
 }
 
 //On teste si value == expected avec des bool
 bool test_equality_bool(bool expected, bool value, char * msg) {
-    if (expected != value)
+    if (expected!=value)
         fprintf(stderr, "ERR: value expected %d ; value computed %d. %s\n", expected, value, msg);
-    return expected == value;
+    return expected==value;
 }
-
-bool test_equality_piece(cpiece expected, cpiece value, char * msg){
-    if (!equals(expected, value)){
+bool test_equality_piece(cpiece expected, cpiece value, char * msg) {
+    if (!equals(expected, value)) {
         fprintf(stderr, "ERR: Pieces are different. %s\n", msg);
     }
+    if(!equals(expected, value)) printf("---------------> equals failed.\n");
     return equals(expected, value);
 }
-    
+
 //création d'un tableau de pièces
 piece pieces[NB_PIECES];
 
 //supprime le tableau de pièces
 void tear_down() {
-    for (int i = 0; i < NB_PIECES; i++)
+    for (int i=0; i<NB_PIECES; i++)
         delete_piece(pieces[i]);
 }
-
 /* config de test
 . . . . . 3
 . . . . . 3
@@ -46,91 +45,81 @@ void tear_down() {
  */
 
 game set_game() {
-    pieces[0] = new_piece_rh(3, 3, true, true);
-    pieces[1] = new_piece_rh(3, 0, true, false);
-    pieces[2] = new_piece_rh(4, 1, true, true);
-    pieces[3] = new_piece_rh(5, 3, false, false);
+    pieces[0]=new_piece_rh(3, 3, true, true);
+    pieces[1]=new_piece_rh(3, 0, true, false);
+    pieces[2]=new_piece_rh(4, 1, true, true);
+    pieces[3]=new_piece_rh(5, 3, false, false);
     return new_game_hr(NB_PIECES, pieces);
 }
-
-
 bool test_new_game_hr() {
-    bool result = true;
-    game g = set_game();
-    result = result && test_equality_int(NB_PIECES, game_nb_pieces(g), "game_nb_pieces");
-    for (int i = 0; i < NB_PIECES; i++) {
-        result = result && equals(pieces[i], game_piece(g, i));
+    bool result=true;
+    game g=set_game();
+    result=result&&test_equality_int(NB_PIECES, game_nb_pieces(g), "game_nb_pieces");
+    for (int i=0; i<NB_PIECES; i++) {
+        result=result&&equals(pieces[i], game_piece(g, i));
     }
     delete_game(g);
+    if(!result) printf("---------------> new_game_hr() failed.\n");
     return result;
 }
 // test play move etnre g (jeu dans lequel seul 0 peut bouger) et gtest(jeu de base)
-
-bool test_play_move(){
-    bool result = true;
-    int nbmove = 1;
-    game g = set_game();
-    cpiece p_test = new_piece_rh(2, 3, true, true);
-    game gtest = set_game();
-    play_move(g, 0, LEFT, 1);           // déplacement vers la gauche de 0
-    play_move(g, 1, DOWN, 1);           // sortie de la grille de 1 : déplacement impossible
-    play_move(g, 2, LEFT, 1);           // intersection de 2 et 1 : déplacement impossible
-    play_move(g, 3, DOWN, 2);           // intersection de 3 et 2 : déplacement impossible
-    result = result && test_equality_int(nbmove, game_nb_moves(g), "game_nb_moves");
-    result = result && !test_equality_piece(p_test, game_piece(g, 0), "play_move de 0");                // seule piece ayant fait un déplacement
-    for (int i = 1; i < game_nb_pieces(g); ++i){
-        result = result && test_equality_piece(game_piece(gtest, i), game_piece(g, i), "play_move");    // déplacements impossible : pieces identiques à la configuration de départ
+bool test_play_move() {
+    bool result=true;
+    int nbmove=0;
+    game g=set_game();
+    cpiece p_test=new_piece_rh(2, 3, true, true);
+    game gtest=set_game();
+    result=result && test_equality_bool(true, play_move(g, 0, LEFT, 1), "play_move camille"); // déplacement vers la gauche de 0
+    result=result && test_equality_bool(false, play_move(g, 1, DOWN, 1), "play_move camille 2"); // sortie de la grille de 1 : déplacement impossible
+    result=result && test_equality_bool(false, play_move(g, 2, LEFT, 1), "play_move camille 3"); // intersection de 2 et 1 : déplacement impossible
+    result=result && test_equality_bool(false, play_move(g, 3, DOWN, 2), "play_move camille 4"); // intersection de 3 et 2 : déplacement impossible
+    if((result=result&&test_equality_int(nbmove, game_nb_moves(g), "game_nb_moves"))==false) printf("---------------> game_nb_move failed.\n");
+    result=result && !test_equality_piece(p_test, game_piece(g, 0), "play_move de 0"); // seule piece ayant fait un déplacement
+    for (int i=1; i<game_nb_pieces(g); ++i) {
+        result=result&&test_equality_piece(game_piece(gtest, i), game_piece(g, i), "play_move"); // déplacements impossible : pieces identiques à la configuration de départ
     }
     tear_down();
     delete_game(g);
+    if(!result) printf("---------------> play_move failed.\n");
     return result;
 }
-
-bool test_copy_game(){
-    bool result = true;
-    game g = set_game();
+bool test_copy_game() {
+    bool result=true;
+    game g=set_game();
     play_move(g, 0, LEFT, 1);
-    game gtest = new_game_hr(NB_PIECES,pieces);
+    game gtest=new_game_hr(NB_PIECES, pieces);
     copy_game(g, gtest);
-    result = result && test_equality_int(game_nb_pieces(g), game_nb_pieces(gtest), "nb_piece copy");
-    for (int i = 0; i < game_nb_pieces(g); ++i){
-        result = result && test_equality_piece(game_piece(gtest, i), game_piece(g, i), "play_move");
+    result=result&&test_equality_int(game_nb_pieces(g), game_nb_pieces(gtest), "nb_piece copy");
+    for (int i=0; i<game_nb_pieces(g); ++i) {
+        result=result&&test_equality_piece(game_piece(gtest, i), game_piece(g, i), "play_move");
     }
     delete_game(g);
     delete_game(gtest);
+    if(!result) printf("---------------> copy_game failed.\n");
     return result;
 }
+int main(int argc, char *argv[]) {
+    bool result=true;
+    game g=set_game();
+    result=result&&test_equality_bool(true, test_new_game_hr(), "new_game_hr"); 
+    result=result&&test_equality_bool(true, test_copy_game(), "copy_game");
+    result=result&&test_equality_bool(true, test_play_move(), "play_move"); //bug
+    play_move(g, 0, LEFT, 2);
+    result=result&&test_equality_bool(true, !game_over_hr(g), "game_over_hr : premier play_move 0");
+    play_move(g, 1, UP, 4);
+    result=result&&test_equality_bool(true, !game_over_hr(g), "game_over_hr : play_move 1");
+    play_move(g, 2, LEFT, 1);
+    result=result&&test_equality_bool(true, !game_over_hr(g), "game_over_hr : play_move 2"); 
+    play_move(g, 3, DOWN, 3);
+    result=result&&test_equality_bool(true, !game_over_hr(g), "game_over_hr : play_move 3");
+    play_move(g, 0, RIGHT, 3);
+    result=result&&test_equality_bool(true, game_over_hr(g), "game_over_hr : deuxième play_move 0");
 
-
-int main (int argc, char *argv[])
-{
-  bool result=true;
-  printf("YOLO?\n");
-
-  game g = set_game();
-  printf("0\n");
-  result = result && test_equality_bool(true, test_new_game_hr(), "new_game_hr");   //retourne l'erreur "ERR: value expected 1 ; value computed 0. new_game_hr"
-  printf("1\n");
-  result = result && test_equality_bool(true, test_copy_game(), "copy_game");
-  printf("2\n");
-  result = result && test_equality_bool(true, test_play_move(), "play_move");
-  play_move(g, 0, LEFT, 2);
-  printf("3\n");
-  result = result && test_equality_bool(true, !game_over_hr(g), "game_over_hr : premier play_move 0");
-  play_move(g, 1, UP, 4);
-  result = result && test_equality_bool(true, !game_over_hr(g), "game_over_hr : play_move 1");
-  play_move(g, 2, LEFT, 1);
-  printf("5\n");
-  result = result && test_equality_bool(true, !game_over_hr(g), "game_over_hr : play_move 2"); //SEGFAULT
-  play_move(g, 3, DOWN, 3);
-  result = result && test_equality_bool(true, !game_over_hr(g), "game_over_hr : play_move 3");
-  play_move(g, 0, RIGHT, 3);
-  result = result && test_equality_bool(true, game_over_hr(g), "game_over_hr : deuxième play_move 0");
-
-  if (result) {
-    printf("Ca marche c: !\n");
-    return EXIT_SUCCESS;
-  }
-  else
-    return EXIT_FAILURE;
+    if (result) {
+        printf("Ca marche c: !\n");
+        return EXIT_SUCCESS;
+    } else {
+        printf("Ton code c'est de la merde\n");
+        return EXIT_FAILURE;
+    }
 }
