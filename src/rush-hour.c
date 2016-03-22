d62c79ddf3661f7f715c6de8daf1cbd2b4748b37
Luc Allio <lucallio@emi.u-bordeaux1.fr>
2016-03-22 11:02:54 +0100

    .

--- src/rush-hour.c
+++ src/rush-hour.c
@@ -1,94 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "game.h"   //inutile mais au cas ou
-#include "piece.h"  //inutile mais au cas ou
-#include "grid.h"
-#include "useful_functions.h"
+#include "piece.h"
+#include "game.h"
+#include "test_functions.h"
 
-int main(void) {
-    piece liste [8];
-    liste[0] = new_piece_rh(0, 3, true, true);
-    liste[1] = new_piece_rh(0, 2, true, true);
-    liste[2] = new_piece_rh(0, 0, true, false);
-    liste[3] = new_piece_rh(1, 1, true, true);
-    liste[4] = new_piece_rh(2, 2, false, false);
-    liste[5] = new_piece_rh(3, 1, true, false);
-    liste[6] = new_piece_rh(4, 0, false, false);
-    liste[7] = new_piece_rh(5, 2, true, false);
-    game game = new_game_hr(8, liste);
-    grid grid = new_grid(game);
-    set_pieces(grid);
-    show_grid(grid);
+int main(void){
+    //actual configuration of the game
+    /*
+     . . . . . .
+     . . 4 . . .
+     0 0 4 . . 7
+     1 1 4 5 6 7
+     2 3 3 5 6 .
+     2 . . . 6 .
+     */
+
+    piece list [8];
+    list[0]=new_piece_rh(0, 3, true, true);
+    list[1]=new_piece_rh(0, 2, true, true);
+    list[2]=new_piece_rh(0, 0, true, false);
+    list[3]=new_piece_rh(1, 1, true, true);
+    list[4]=new_piece_rh(2, 2, false, false);
+    list[5]=new_piece_rh(3, 1, true, false);
+    list[6]=new_piece_rh(4, 0, false, false);
+    list[7]=new_piece_rh(5, 2, true, false);
+    game game=new_game_hr(8, list);
+    printf("\e[2J\e[H"); //clean the shell
+    printf("***** Welcome in RushHour ! *****\nYou need to drive the 0 car to the right !\nGood luck !\n\n");
+    show_grid(game);
 
     int piece_num;
     dir d;
     int num;
     int distance;
     int test;
-    while (!(game_over_hr(game))) {
+    while (!(game_over_hr(game))){
         piece_num=-1;
         d=-1;
         distance=-1;
-        test = false;
-        
-        printf("Veuillez saisir le numéro de la pièce à déplacer :\n");
-        //commenter plus tard ces fonctions
-        while(!test){
-            if (scanf("%d", &piece_num) == 1) { // on récupère la valeur donnée en entrée par l'utilisateur et on vérifie si c'est un entier
-                if(piece_num>=0 && piece_num<game_nb_pieces(game)) // si la valeur entrée est un numéro valable de pièce
-                  test=true;
-                else
-                    printf("Entrée invalide: veuillez saisir un entier entre 0 et %d.\n", game_nb_pieces(game)-1);
-            }
-            else{
-                printf("Entrée invalide: veuillez saisir un entier entre 0 et %d.\n", game_nb_pieces(game)-1);
-                while(fgetc(stdin)!='\n'); // par contre j'ai pas compris à quoi sert celle ligne
-            }
-        }
         test=false;
-        
-        printf("Quelle direction ? (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
-        while (!test) {
-            if (scanf("%d", &num) == 1) { // on récupère la valeur donnée en entrée par l'utilisateur et on vérifie si c'est un entier
-                if (num == 1) { // la valeur de direction doit être 1, 2, 3, ou 4 (je propose d'ailleurs de changer pour 2=DOWN, 4=LEFT, 6=RIGHT, 8=UP)
-                    d = UP;
-                    test = true;
-                } else if (num == 2) {
-                    d = DOWN;
-                    test = true;
-                } else if (num == 3) {
-                    d = LEFT;
-                    test = true;
-                } else if (num == 4) {
-                    d = RIGHT;
-                    test = true;
-                } else {
-                    printf("Entrée invalide: veuillez saisir un entier entre 1 et 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
-                }
-            }
-            else{
-                printf("Entrée invalide: veuillez saisir un entier entre 1 et 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
-                while(fgetc(stdin)!='\n');
-            }
-        }
-        test=false;
-        printf("De combien de cases ?\n");
-        while(!test){
-            if (scanf("%d", &distance)==1){ // on récupère la valeur donnée en entrée par l'utilisateur et on vérifie si c'est un entier
-                if(distance>0) // la distance doit être positive
+        printf("Please input the number of the piece you want to move :\n");
+        while (!test){
+            if (scanf("%d", &piece_num)==1){ //prompt the user for an int AND check if the input is an int
+                if (piece_num>=0&&piece_num<game_nb_pieces(game)) //check if the number is one of the piece
                     test=true;
                 else
-                    printf("Entrée invalide: veuillez saisir un entier supérieur à 0.\n");
-            }
-            else{
-                printf("Entrée invalide: veuillez saisir un entier superieur à 0.\n");
-                while(fgetc(stdin)!='\n');
+                    printf("Invalid input: please write an integer between 0 and %d.\n", game_nb_pieces(game)-1);
+            } else{ //case when the input isn't an int
+                printf("Invalid input: please write an integer between 0 and %d.\n", game_nb_pieces(game)-1);
+                while (fgetc(stdin)!='\n');
+                //if scanf works, it puts the input in the stdin buffer, convert it in an integer, removes it from the buffer and returns 1.
+                //but if it failed, the input will be left in the stdin buffer, so you need to "clean" the buffer,
+                //otherwise scanf will see that stdin isn't empty and won't ask for user input, will return 0, and loop again and again
             }
         }
-        move(grid, piece_num, d, distance);
-    }
+        test=false;
 
-    if (game_over_hr(game)) printf("Game over. Score : %d", game_nb_moves(game));
+        printf("Which direction ? (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+        while (!test){
+            if (scanf("%d", &num)==1){
+                if (num==1){
+                    d=UP;
+                    test=true;
+                } else if (num==2){
+                    d=DOWN;
+                    test=true;
+                } else if (num==3){
+                    d=LEFT;
+                    test=true;
+                } else if (num==4){
+                    d=RIGHT;
+                    test=true;
+                } else{
+                    printf("Invalid input: please write an integer between 1 and 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+                }
+            } else{
+                printf("Invalid input: please write an integer between 1 and 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+                while (fgetc(stdin)!='\n');
+            }
+        }
+        test=false;
+        printf("How many times ?\n");
+        while (!test){
+            if (scanf("%d", &distance)==1){
+                if (distance>0)
+                    test=true;
+                else
+                    printf("Invalid input: please write an integer greater than 0.\n");
+            } else{
+                printf("Invalid input: please write an integer greater than 0.\n");
+                while (fgetc(stdin)!='\n');
+            }
+        }
+        printf("\e[2J\e[H");
+        play_move(game, piece_num, d, distance);
+        show_grid(game);
+    }
+    printf("Game is over. Score : %d", game_nb_moves(game));
     return EXIT_SUCCESS;
 }
