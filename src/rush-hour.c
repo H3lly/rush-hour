5ea7e03ba22cc6319b65cda6700f7532b0393979
Camille Aussignac <camille.aussignac@gmail.com>
2016-03-30 00:41:09 +0200

    faire rush-hour test pour qu'il joue automatiquement

--- src/rush-hour.c
+++ src/rush-hour.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "piece.h"
 #include "game.h"
 #include "test_functions.h"
 
-int main(void){
+int main(int argc, char** argv){
     //actual configuration of the game
     /*
      . . . . . .
@@ -26,77 +27,100 @@ int main(void){
     list[6]=new_piece_rh(4, 0, false, false);
     list[7]=new_piece_rh(5, 2, true, false);
     game game=new_game_hr(8, list);
-    printf("\e[2J\e[H"); //clean the shell
-    printf("***** Welcome in RushHour ! *****\nYou need to drive the 0 car to the right !\nGood luck !\n\n");
-    show_grid(game);
+    if(argc==2 && !strcmp(argv[1],"test")){
+        play_move(game, 7, UP, 2);
+        play_move(game, 6, UP, 3);
+        play_move(game, 5, UP, 3);
+        play_move(game, 4, UP, 1);
+        play_move(game, 1, RIGHT, 3);
+        play_move(game, 3, RIGHT, 2);
+        play_move(game, 4, DOWN, 3);
+        play_move(game, 0, RIGHT, 1);
+        play_move(game, 2, UP, 4);
+        play_move(game, 0, LEFT, 1);
+        play_move(game, 4, UP, 3);
+        play_move(game, 1, LEFT, 3);
+        play_move(game, 3, LEFT, 3);
+        play_move(game, 6, DOWN, 3);
+        play_move(game, 4, DOWN, 3);
+        play_move(game, 0, RIGHT, 4);
+        delete_game(game);
+        return 1;
+    }
+    else{
+        printf("\e[2J\e[H");//clean the shell
+        printf("***** Welcome in RushHour ! *****\nYou need to drive the 0 car to the right !\nGood luck !\n\n");
+        show_grid(game);
 
-    int piece_num;
-    dir d;
-    int num;
-    int distance;
-    int test;
-    while (!(game_over_hr(game))){
-        piece_num=-1;
-        d=-1;
-        distance=-1;
-        test=false;
-        printf("Please input the number of the piece you want to move :\n");
-        while (!test){
-            if (scanf("%d", &piece_num)==1){ //prompt the user for an int AND check if the input is an int
-                if (piece_num>=0&&piece_num<game_nb_pieces(game)) //check if the number is one of the piece
-                    test=true;
-                else
+        int piece_num;
+        dir d;
+        int num;
+        int distance;
+        int test;
+        while (!(game_over_hr(game))){
+            piece_num=-1;
+            d=-1;
+            distance=-1;
+            test=false;
+            printf("Please input the number of the piece you want to move :\n");
+            while (!test){
+                if (scanf("%d", &piece_num)==1){ //prompt the user for an int AND check if the input is an int
+                    if (piece_num>=0&&piece_num<game_nb_pieces(game)) //check if the number is one of the piece
+                        test=true;
+                    else
+                        printf("Invalid input: please write an integer between 0 and %d.\n", game_nb_pieces(game)-1);
+                } else{ //case when the input isn't an int
                     printf("Invalid input: please write an integer between 0 and %d.\n", game_nb_pieces(game)-1);
-            } else{ //case when the input isn't an int
-                printf("Invalid input: please write an integer between 0 and %d.\n", game_nb_pieces(game)-1);
-                while (fgetc(stdin)!='\n');
-                //if scanf works, it puts the input in the stdin buffer, convert it in an integer, removes it from the buffer and returns 1.
-                //but if it failed, the input will be left in the stdin buffer, so you need to "clean" the buffer,
-                //otherwise scanf will see that stdin isn't empty and won't ask for user input, will return 0, and loop again and again
+                    while (fgetc(stdin)!='\n');
+                    //if scanf works, it puts the input in the stdin buffer, convert it in an integer, removes it from the buffer and returns 1.
+                    //but if it failed, the input will be left in the stdin buffer, so you need to "clean" the buffer,
+                    //otherwise scanf will see that stdin isn't empty and won't ask for user input, will return 0, and loop again and again
+                }
             }
-        }
-        test=false;
+            test=false;
 
-        printf("Which direction ? (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
-        while (!test){
-            if (scanf("%d", &num)==1){
-                if (num==1){
-                    d=UP;
-                    test=true;
-                } else if (num==2){
-                    d=DOWN;
-                    test=true;
-                } else if (num==3){
-                    d=LEFT;
-                    test=true;
-                } else if (num==4){
-                    d=RIGHT;
-                    test=true;
+            printf("Which direction ? (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+            while (!test){
+                if (scanf("%d", &num)==1){
+                    if (num==1){
+                        d=UP;
+                        test=true;
+                    } else if (num==2){
+                        d=DOWN;
+                        test=true;
+                    } else if (num==3){
+                        d=LEFT;
+                        test=true;
+                    } else if (num==4){
+                        d=RIGHT;
+                        test=true;
+                    } else{
+                        printf("Invalid input: please write an integer between 1 and 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+                    }
                 } else{
                     printf("Invalid input: please write an integer between 1 and 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
+                    while (fgetc(stdin)!='\n');
                 }
-            } else{
-                printf("Invalid input: please write an integer between 1 and 4. (1=UP, 2=DOWN, 3=LEFT, 4=RIGHT)\n");
-                while (fgetc(stdin)!='\n');
             }
-        }
-        test=false;
-        printf("How many times ?\n");
-        while (!test){
-            if (scanf("%d", &distance)==1){
-                if (distance>0)
-                    test=true;
-                else
+            test=false;
+            printf("How many times ?\n");
+            while (!test){
+                if (scanf("%d", &distance)==1){
+                    if (distance>0)
+                        test=true;
+                    else
+                        printf("Invalid input: please write an integer greater than 0.\n");
+                } else{
                     printf("Invalid input: please write an integer greater than 0.\n");
-            } else{
-                printf("Invalid input: please write an integer greater than 0.\n");
-                while (fgetc(stdin)!='\n');
+                    while (fgetc(stdin)!='\n');
+                }
             }
+            printf("\e[2J\e[H");
+            play_move(game, piece_num, d, distance);
+            show_grid(game);
         }
-        printf("\e[2J\e[H");
-        play_move(game, piece_num, d, distance);
-        show_grid(game);
-    }
-    printf("Game is over. Score : %d", game_nb_moves(game));
+        printf("Game is over. Score : %d", game_nb_moves(game));
+    delete_game(game);
     return EXIT_SUCCESS;
+    }
 }
