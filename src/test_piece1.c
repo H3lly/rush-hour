ff5e3d842bb190c2d2847d1485959a64649947c0
Camille Aussignac <camille.aussignac@gmail.com>
2016-04-05 16:37:01 +0200

    test_piece test_new_piece

--- src/test_piece1.c
+++ src/test_piece1.c
@@ -29,90 +29,73 @@ void set_up(){
 }
 
 void tear_down(){
-    for (int i=0; i < NB_PIECES; i++)
+    for (int i=0;i<NB_PIECES;i++)
         delete_piece(pieces[i]);
 }
+
 bool test_new_piece(){
     set_up();
-    bool result = true;
-    result = test_equality_int(3, get_x(piece[0]), "get_x in test_new_piece");
-    result = test_equality_int(3, get_y(piece[0]), "get_y in test_new_piece")&&result;
-    result = test_equality_int(2, get_width(piece[0]), "get_width in test_new_piece")&&result;
-    result = test_equality_int(1, get_height(piece[0]), "get_height in test_new_piece")&&result;
-    result = test_equality_bool(2, get_width(piece[0]), "get_width in test_new_piece")&&result;
+    bool result=true;
+    result=test_equality_int(3, get_x(pieces[0]), "get_x in test_new_piece");
+    result=test_equality_int(3, get_y(pieces[0]), "get_y in test_new_piece")&&result;
+    result=test_equality_int(2, get_width(pieces[0]), "get_width in test_new_piece")&&result;
+    result=test_equality_int(1, get_height(pieces[0]), "get_height in test_new_piece")&&result;
+    result=test_equality_bool(true, is_horizontal(pieces[0]), "is_horizontal in test_new_piece")&&result;
+    result=test_equality_bool(true, is_small(pieces[0]), "is_small in test_new_piece")&&result;
+    result=test_equality_bool(true, can_move_x(pieces[0]), "can_move_x in test_new_piece")&&result;
+    result=test_equality_bool(false, can_move_y(pieces[0]), "can_move_y in test_new_piece")&&result;
+    tear_down();
+    return result;
 }
-//bool test_new_piece(){
-//    bool result=true;
-//    for (int x=0; x < 5; x++)
-//        for (int y=0; y < 5; y++)
-//            for (bool small=false; !small; small= !small)
-//                for (bool horizontal=false; !horizontal; horizontal= !horizontal){
-//                    int size;
-//                    if (small)
-//                        size=2; // code non executé selon gcov
-//                    else
-//                        size=3;
-//                    piece p=new_piece_rh(x, y, small, horizontal);
-//                    result=result && test_equality_int(x, get_x(p), "get_x");
-//                    result=result && test_equality_int(y, get_y(p), "get_y");
-//                    if (horizontal){
-//                        result=result && test_equality_int(1, get_height(p), "get_height"); // code non executé selon gcov
-//                        result=result && test_equality_int(size, get_width(p), "get_width"); // code non executé selon gcov
-//                    } else{
-//                        result=result && test_equality_int(size, get_height(p), "get_height");
-//                        result=result && test_equality_int(1, get_width(p), "get_width");
-//                    }
-//                    delete_piece(p);
-//                }
-//    return result;
-//}
+
 bool test_intersect(){
     bool result=true;
     set_up();
-    for (int i=0; i < NB_PIECES; i++)
-        for (int j=0; j < NB_PIECES; j++){
-            result=result && test_equality_bool(i == j, intersect(pieces[i], pieces[j]), "intersect");
+    for (int i=0;i<NB_PIECES;i++)
+        for (int j=0;j<NB_PIECES;j++){
+            result=result&&test_equality_bool(i==j, intersect(pieces[i], pieces[j]), "intersect");
         }
 
     piece pb_piece1=new_piece_rh(3, 3, false, false);
     piece pb_piece2=new_piece_rh(3, 1, false, false);
-    result=result && test_equality_bool(true, intersect(pieces[0], pb_piece1), "intersect pb1");
-    result=result && test_equality_bool(true, intersect(pb_piece2, pb_piece1), "intersect pb2");
+    result=result&&test_equality_bool(true, intersect(pieces[0], pb_piece1), "intersect pb1");
+    result=result&&test_equality_bool(true, intersect(pb_piece2, pb_piece1), "intersect pb2");
     delete_piece(pb_piece1);
     delete_piece(pb_piece2);
     tear_down();
     return result;
 }
+
 bool test_move(){
     bool result=true;
     piece p=new_piece_rh(0, 0, true, true);
     set_up();
-    for (int dist=1; dist < NB_PIECES; dist++)
-        for (int i=0; i < NB_PIECES; i++){
+    for (int dist=1;dist<NB_PIECES;dist++)
+        for (int i=0;i<NB_PIECES;i++){
             copy_piece(pieces[i], p);
             move_piece(p, LEFT, dist);
             if (is_horizontal(pieces[i]))
-                result=result && test_equality_int(get_x(pieces[i]) - dist, get_x(p), "move LEFT");
+                result=result&&test_equality_int(get_x(pieces[i])-dist, get_x(p), "move LEFT");
             else
-                result=result && test_equality_int(get_x(pieces[i]), get_x(p), "move LEFT");
+                result=result&&test_equality_int(get_x(pieces[i]), get_x(p), "move LEFT");
             copy_piece(pieces[i], p);
             move_piece(p, RIGHT, dist);
             if (is_horizontal(pieces[i]))
-                result=result && test_equality_int(get_x(pieces[i]) + dist, get_x(p), "move RIGHT");
+                result=result&&test_equality_int(get_x(pieces[i])+dist, get_x(p), "move RIGHT");
             else
-                result=result && test_equality_int(get_x(pieces[i]), get_x(p), "move RIGHT");
+                result=result&&test_equality_int(get_x(pieces[i]), get_x(p), "move RIGHT");
             copy_piece(pieces[i], p);
             move_piece(p, UP, dist);
             if (!is_horizontal(pieces[i]))
-                result=result && test_equality_int(get_y(pieces[i]) + dist, get_y(p), "move UP");
+                result=result&&test_equality_int(get_y(pieces[i])+dist, get_y(p), "move UP");
             else
-                result=result && test_equality_int(get_y(pieces[i]), get_y(p), "move UP");
+                result=result&&test_equality_int(get_y(pieces[i]), get_y(p), "move UP");
             copy_piece(pieces[i], p);
             move_piece(p, DOWN, dist);
             if (!is_horizontal(pieces[i]))
-                result=result && test_equality_int(get_y(pieces[i]) - dist, get_y(p), "move DOWN");
+                result=result&&test_equality_int(get_y(pieces[i])-dist, get_y(p), "move DOWN");
             else
-                result=result && test_equality_int(get_y(pieces[i]), get_y(p), "move DOWN");
+                result=result&&test_equality_int(get_y(pieces[i]), get_y(p), "move DOWN");
 
 
         }
@@ -121,34 +104,36 @@ bool test_move(){
     return result;
     return false;
 }
+
 bool test_copy(){
     piece p=new_piece_rh(0, 0, true, true);
     bool result=true;
     set_up();
-    for (int i=0; i < NB_PIECES; i++){
+    for (int i=0;i<NB_PIECES;i++){
         copy_piece(pieces[i], p);
-        result=result && test_equality_int(get_height(pieces[i]), get_height(p), "copy get_height");
-        result=result && test_equality_int(get_width(pieces[i]), get_width(p), "copy get_width");
-        result=result && test_equality_int(get_x(pieces[i]), get_x(p), "copy get_x");
-        result=result && test_equality_int(get_y(pieces[i]), get_y(p), "copy get_y");
-        result=result && test_equality_bool(is_horizontal(pieces[i]), is_horizontal(p), "copy is_horizontal");
+        result=result&&test_equality_int(get_height(pieces[i]), get_height(p), "copy get_height");
+        result=result&&test_equality_int(get_width(pieces[i]), get_width(p), "copy get_width");
+        result=result&&test_equality_int(get_x(pieces[i]), get_x(p), "copy get_x");
+        result=result&&test_equality_int(get_y(pieces[i]), get_y(p), "copy get_y");
+        result=result&&test_equality_bool(is_horizontal(pieces[i]), is_horizontal(p), "copy is_horizontal");
     }
     tear_down();
     delete_piece(p);
     return result;
 }
+
 int main(int argc, char *argv[]){
     bool result=true;
 
-    result=result && test_equality_bool(true, test_new_piece(), "new_piece");
-    result=result && test_equality_bool(true, test_intersect(), "intersect");
-    result=result && test_equality_bool(true, test_move(), "move");
-    result=result && test_equality_bool(true, test_copy(), "copy");
+    result=result&&test_equality_bool(true, test_new_piece(), "new_piece");
+    result=result&&test_equality_bool(true, test_intersect(), "intersect");
+    result=result&&test_equality_bool(true, test_move(), "move");
+    result=result&&test_equality_bool(true, test_copy(), "copy");
 
     if (result){
         printf("Yay !\n");
         return EXIT_SUCCESS;
     } else
         printf("Meh\n");
-        return EXIT_FAILURE;
+    return EXIT_FAILURE;
 }
