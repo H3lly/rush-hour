b781a45455ddcc6ef3eb1f33cd60493661b25e2a
nyandry <anraharison@hotmail.fr>
2016-04-15 15:32:48 +0200

    quasi fin du solveur

--- src/solveur2.c
+++ src/solveur2.c
@@ -6,50 +6,113 @@
 #include "game.h"
 #include "test_functions.h"
 
-struct TreeGame{
+typedef struct tree_game_s* tree_game;
+
+
+struct tree_game_s{
 	game node;
 	game *children;
-	int iChildren;
+	int ind_children;
 };
 
-TreeGame createTree(g){
-	TreeGame t = malloc(sizeof (struct TreeGame));
+struct game_s{
+    int width;
+    int height;
+    int nb_pieces;
+    int nb_moves;
+    piece *piece_list;
+};
+
+tree_game create_tree(game g){
+	tree_game t = malloc(sizeof (struct tree_game_s));
 	t->node = g;
-	t->iChildren = -1;
+	t->children = NULL;
+	t->ind_children = -1;
+	return t;
 }
 
-game getNode(){
-	return t->base;
+game get_node(tree_game t){
+	return t->node;
 }
 
-void addChild(t, g){
-	if (t->iChildren == -1){
-		t->children = malloc(sizeof (struct game));
+void add_child(tree_game t, game g){
+	if (t->ind_children == -1){
+		t->children = malloc(sizeof (struct game_s));
 	}
 	else{
-		t->children = realloc(t->children, (iChildren+2)*sizeof(struct game));
+		t->children = realloc(t->children, (t->ind_children+2)*sizeof(struct game_s));
 	}
-	t->child[++iChild] = g;
+	t->children[++t->ind_children] = g;
 }
-/*
-game solve(TreeGame t, game solved){
-	game g = getNode();
+
+bool has_child(tree_game t){
+	return (t->ind_children != -1);
+}
+
+
+game solve(tree_game t, dir* prev){
+	game gc;
+	cgame g = get_node(t);
 	for (int p = 0; p<g->nb_pieces; ++p){
-		if can_move_x()
+		if (can_move_x(g->piece_list[p])){
+			if (prev[0] != LEFT){
+				copy_game(g, gc);
+				play_move(gc, p, LEFT, 1);
+				add_child(t, gc);
+				if (game_over_hr(gc)){		//game over pour ane rouge?
+					return gc;
+				}
+				prev[0] = LEFT;
+			}
+			if (prev[1] != RIGHT){
+				copy_game(g, gc);
+				play_move(gc, p, RIGHT, 1);
+				add_child(t, gc);
+				if (game_over_hr(gc)){		//game over pour ane rouge?
+					return gc;
+				}
+				prev[1] = RIGHT;
+
+			}
+		}
+		if (can_move_y(g->piece_list[p])){
+			if (prev[2] != UP){
+				copy_game(g, gc);
+				play_move(gc, p, UP, 1);
+				add_child(t, gc);
+				if (game_over_hr(gc)){		//game over pour ane rouge?
+					return gc;
+				}
+				prev[2] = UP;
+			}
+			if (prev[3] != DOWN){
+				copy_game(g, gc);
+				play_move(gc, p, DOWN, 1);
+				add_child(t, gc);
+				if (game_over_hr(gc)){		//game over pour ane rouge?
+					return gc;
+				}
+				prev[3] = DOWN;
+			}
+		}
 	}
+	if (has_child(t)){
+		for (int ge = 0; ge < t->ind_children+2; ++ge){
+			solve(t, prev);
+		}
+	}
+	return NULL;
 }
-*/
-bool hasChild(t){
-	return (t->iChildren != -1);
-}
-/*
-int resultSolve(g){
-	t = createTree(g);
-	game solved == NULL;
-	solve(t, solved);
+
+
+int result_solve(game g){
+	tree_game t;
+	t = create_tree(g);
+	dir prev[4] = {RIGHT, LEFT, LEFT, LEFT};
+	game solved = NULL;
+	solved = solve(t, prev);
 	if (solved == NULL){
 		return -1;
 	}
-	return solved->nbMoves;
+	return solved->nb_moves;
 }
-*/
